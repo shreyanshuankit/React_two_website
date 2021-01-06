a1cf426abdde0990f76b4e7c648b5c78aee27237
Shreyanshu Santosh <shreyanshuankit@users.noreply.github.com>
2021-01-06 10:42:24 +0530

    Template modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./navbar";
+import Header from "./header";
+import Portfolio from "./portfolio";
+import About from "./about";
+import Contact from "./contact";
+import Footer from "./footer";
+import CopyRight from "./copyright";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<NavBar name="Shreyanshu" />
+			<Header />
+			<Portfolio />
+			<About />
+			<Contact />
+			<Footer />
+			<CopyRight />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/copyright.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CopyRight = () => {
+	return (
+		<div>
+			<div class="copyright py-4 text-center text-white">
+				<div class="container">
+					<small>Copyright © Your Website 2020</small>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CopyRight;
